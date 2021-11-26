c3799f8c59a25eda02abed54cd6f57b9e968eebd
Nina <morville5audrey@gmail.com>
2021-11-26 05:11:35 +0100

    add crud method to images api

---
+++ controllers/photosController.js
@@ -0,0 +1,77 @@
+//modules necessaires:
+const express = require('express');
+const router = express.Router();
+const ObjectId =require('mongoose').Types.ObjectId;
+
+
+
+ 
+
+//recuperation des fichiers externes necessaires:
+const { PhotosModel } = require('../models/photosModel');
+
+//déclaration des fonctions du router methode CRUD:
+//pour lire les articles:
+router.get('/', (req, res) => {
+    PhotosModel.find((err, docs) => {
+        if(!err) res.send (docs);
+        else console.log ('error:'+ err);
+    })
+});
+
+
+//pour créer des articles:
+router.post('/',(req, res) => {
+    const newRecord = new PhotosModel({
+        name: req.body.name,
+        description: req.body.description,
+        imageURL: req.body.imageURL
+    });
+    newRecord.save((err, docs) => {
+         if (!err) res.send (docs);
+        else console.log ('error creating new data:' +err)
+    })
+} );
+
+
+//pour modifier des articles:
+router.put('/:id', (req,res) =>{
+    if(!ObjectId.isValid(req.params.id)
+    )
+    return res.status(400).send('id non valide'+req.params.id)
+
+    const photoModifiée = {
+        name: req.body.name,
+        description: req.body.description,
+        imageURL: req.body.imageURL
+    };
+
+    PhotosModel.findByIdAndUpdate(
+        req.params.id,
+        {$set: photoModifiée},
+        {new: true},
+        (err, docs) =>{
+            if(!err) res.send(docs);
+            else console.log('update error:'+err);
+        }
+    )
+});
+
+//pour effacer un article:
+router.delete('/:id', (req,res) => {
+    if(!ObjectId.isValid(req.params.id) )
+    return res.status(404).send('detete error:' +err)
+
+    PhotosModel.findByIdAndRemove(
+        req.params.id, 
+        (err, docs) =>{
+        if(!err)res.send(docs);
+        else console.log('delete error:'+err)
+        }
+    )
+});
+
+
+    
+//exportation du router:
+module.exports = router
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+/*
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
@@ -24,6 +24,7 @@ const upload = multer({storage: fileStorageEngine});
 
 
 
+
 app.post('/single', upload.single('image'), (req, res)=> {
     console.log(req.file);
     res.send('Single image upload success')
@@ -37,4 +38,37 @@ app.post('/multiple', upload.array('images',3), (req, res)=> {
 
 
 
+
 app.listen (5000, ()=> console.log('listening on port 5000'));
+
+*/
+
+
+
+ //modules necessaires:
+ const express = require('express');
+ const app = express();
+ const mongoose =require('mongoose');
+ const cors = require('cors');
+
+
+ //récupération des fichiers externes necessaire:
+ require ("./models/dbConfig");
+ const photosControl = require('./controllers/photosController');
+
+
+//middlewares:
+//pour lire (parser) le format json mis dans la methode post 
+ app.use(express.json() );
+//*si on veut gerer le middleware d'encodage d'url pour eviter les valeurs undefined 
+ //app.use(express.urlencoded({extended: false}) );
+ //*en cas de warning
+ //mongoose.set('useFindAndModify', false);
+ //pour gerer l'accés à l'api au public
+ //app.use(cors({origin: '//adresse du site a qui l'ont veut donner accès'}))
+ app.use(cors())
+ app.use('/', photosControl);
+ 
+
+ //lancement du port:
+ app.listen(5500, () => console.log("Server listening on port 5500") );
